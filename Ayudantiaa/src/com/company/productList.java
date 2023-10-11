6d361bcf0f1bdd592830b30dcbe60763eb87577f
Ayudantia <https://github.com/MauricioAchc/Ayudantia.git>
2023-10-11 17:44:50 -0300

    Clases terminadas

--- Ayudantiaa/src/com/company/productList.java
+++ Ayudantiaa/src/com/company/productList.java
@@ -7,25 +7,49 @@ public class productList {
 
     public productList(int maxProducts) {
         this.maxProducts = maxProducts;
+        products = new Product[maxProducts];
+        quantityProdcuts = 0;
     }
 
-    public boolean add(Product product){
+    public void add(Product product){
+        products[quantityProdcuts] = product;
+        quantityProdcuts++;
 
     }
 
-    public Product searchByid(int id){
 
+    public boolean searchByid(int id){
+        for(int i = 0; i < quantityProdcuts; i++){
+            if(products[i].getId() == id){
+                return true;
+            }
+        }
+        return false;
     }
 
     public Product get(int index){
+        return products[index];
 
     }
 
     public boolean deleteByid(int id){
-
+        for(int i = 0; i < quantityProdcuts; i++){
+            if(products[i].getId() == id){
+                products[i] = null;
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean existByName(String name){
-
+        for(int i = 0; i < quantityProdcuts; i++){
+            if(products[i].getName().equalsIgnoreCase(name)){
+                return true;
+            }
+        }
+        return false;
     }
+
+
 }
